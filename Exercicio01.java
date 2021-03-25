78910e393e12efe73668c3f978c33c6821398ccc
DaniloCanali <80927864+DaniloCanali@users.noreply.github.com>
2021-03-24 21:09:23 -0300

    Identação ex 1 - Danilo Câmara Canali - RA:1902971

--- Exercicio01.java
+++ Exercicio01.java
@@ -10,16 +10,16 @@ import javax.swing.WindowConstants;
 public class Exercicio01 extends JFrame {
 
 public Exercicio01() {
-JMenuBar barraMenu = new JMenuBar();
-JMenuItem opCliente, fechar;
-setTitle("Menu Cadastro");
-setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-setSize( 300,100 );   
-JMenu exers = new JMenu("Cadastrar");
-opCliente = exers.add("Veículos");
-exers.addSeparator();
-fechar = exers.add("Fechar");
-barraMenu.add(exers);
+	JMenuBar barraMenu = new JMenuBar();
+	JMenuItem opCliente, fechar;
+	setTitle("Menu Cadastro");
+	setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	setSize( 300,100 );   
+	JMenu exers = new JMenu("Cadastrar");
+	opCliente = exers.add("Veículos");
+	exers.addSeparator();
+	fechar = exers.add("Fechar");
+	barraMenu.add(exers);
 
 opCliente.addActionListener(new ActionListener() {
 public void actionPerformed(ActionEvent arg0) {
@@ -33,15 +33,15 @@ System.exit(0);
 }
 }
 });
-setJMenuBar(barraMenu);
-setVisible( true );
+	setJMenuBar(barraMenu);
+	setVisible( true );
 }
 
 public static void main(String[] args){
-Exercicio01 umaJanela = new Exercicio01();
-umaJanela.setBounds(50,100,400,150);
-umaJanela.setResizable(false);
-umaJanela.setLocationRelativeTo(null);
-umaJanela.setVisible(true);
+	Exercicio01 umaJanela = new Exercicio01();
+	umaJanela.setBounds(50,100,400,150);
+	umaJanela.setResizable(false);
+	umaJanela.setLocationRelativeTo(null);
+	umaJanela.setVisible(true);
 };
 }
